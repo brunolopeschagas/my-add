799f3a8da9258337979dbc2957fa59d1f853b0cc
Bruno <bruno.chagas@gmail.com>
2022-07-20 11:12:44 -0300

    listando users github com campo de pesquisa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter } from 'rxjs';
 import { GitHubService } from './git-hub-data/github.service';
 import { GitHubUser } from './git-hub-data/githubuser';
 
@@ -6,20 +8,26 @@ import { GitHubUser } from './git-hub-data/githubuser';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  
+
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
 
-  users:GitHubUser[] = []
+  searchControl = new FormControl();
+  users: GitHubUser[] = []
 
-  constructor(private _gitHubServices:GitHubService){
+  constructor(private _gitHubServices: GitHubService) {
     this._gitHubServices.getGitHubData('brunolopeschagas')
-    .subscribe(data => {
-      this.users = data.items;
-      console.log(data.items);
-    });
+      .subscribe(data => {
+        this.users = data.items;
+        console.log(data.items);
+      });
   }
-
-
-
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(filter(text => text.length >= 3), debounceTime(400), distinctUntilChanged())
+      .subscribe(value => {
+        this._gitHubServices.getGitHubData(value)
+          .subscribe(data => { this.users = data.items; })
+      });
+  }
 }
